ad535165bc4cc8652a17539e1a60b6e9946d7756
Irina Gorges <r3nategr4nate@gmail.com>
2025-10-09 15:39:31 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Landingpage } from './main-content/landingpage/landingpage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landingpage }
+];
---
+++ src/app/main-content/landingpage/landingpage.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [CommonModule],
+  templateUrl: './landingpage.html',
+  styleUrl: './landingpage.scss'
+})
+export class Landingpage {
+
+}
